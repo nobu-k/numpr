8d3a9ad25aa0400e2516b9e93f3034cf558a60b2
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2020-07-21 20:35:49 -0700

    Improve vec initialization

--- numpr/src/board.rs
+++ numpr/src/board.rs
@@ -55,11 +55,7 @@ mod tests {
 
     #[test]
     fn test_new() {
-        let n: Vec<u8> = [0 as u8; SIZE]
-            .iter()
-            .enumerate()
-            .map(|(i, _)| (i * 2) as u8)
-            .collect();
+        let n: Vec<u8> = (1..=SIZE).map(|i| (i * 2) as u8).collect();
 
         let b = Board::new(&n);
         assert_eq!(b.numbers.len(), SIZE);
