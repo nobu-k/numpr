8b5f59c391b26f5cafd96fe467b527b7ba28b876
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2020-07-21 21:04:39 -0700

    Test Board.get

--- numpr/src/board.rs
+++ numpr/src/board.rs
@@ -17,8 +17,8 @@ impl Board {
         return Board { numbers: b };
     }
 
-    pub fn get(self, x: usize, y: usize) -> Option<u8> {
-        if x > WIDTH || y > HEIGHT {
+    pub fn get(&self, x: usize, y: usize) -> Option<u8> {
+        if x >= WIDTH || y >= HEIGHT {
             return None;
         }
 
@@ -62,5 +62,27 @@ mod tests {
         let b = Board::new(&n);
         assert_eq!(b.numbers.len(), SIZE);
         assert!(b.numbers.iter().eq(n.iter()));
+
+        for x in 0..9 {
+            for y in 0..9 {
+                let expected = n[y * HEIGHT + x];
+                if let Some(v) = b.get(x, y) {
+                    assert_eq!(n[y * HEIGHT + x], v);
+                } else {
+                    assert_eq!(expected, 0);
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn get_out_of_bounds() {
+        let b = Board::new(&[1; SIZE]);
+
+        assert_eq!(Some(1), b.get(0, 0));
+        assert_eq!(None, b.get(WIDTH, 0));
+        assert_eq!(None, b.get(1000, 0));
+        assert_eq!(None, b.get(0, HEIGHT));
+        assert_eq!(None, b.get(0, 1000));
     }
 }
