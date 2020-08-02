7030c1aa6f5531bf5108dd5e5816b76e1f5ed086
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2020-08-02 02:45:54 -0700

    Remove popcounts from HueristicSolver (bench -12%)

--- numpr/src/solvers/heuristic.rs
+++ numpr/src/solvers/heuristic.rs
@@ -6,7 +6,6 @@ use crate::Solver;
 
 pub struct HeuristicSolver {
     masks: [u16; SIZE],
-    popcnts: [u8; SIZE],
 }
 
 const LOG_TABLE_LOW: [u8; 9] = [0, 1, 2, 0, 3, 0, 0, 0, 4];
@@ -16,7 +15,6 @@ impl HeuristicSolver {
     pub fn new() -> Self {
         Self {
             masks: [0b11_1111_1110; SIZE],
-            popcnts: [9; SIZE],
         }
     }
 
@@ -25,8 +23,7 @@ impl HeuristicSolver {
         for (pt, n) in b.iter().filter(|(_, n)| n.is_some()) {
             cnt += 1;
             let n = n.unwrap();
-            self.masks[pt.index()] = 0;
-            self.popcnts[pt.index()] = 10;
+            self.masks[pt.index()] = 1;
             self.dec(pt, n)?;
         }
         Ok(SIZE - cnt)
@@ -34,39 +31,30 @@ impl HeuristicSolver {
 
     fn set(&mut self, b: &mut Board, pt: Pt, n: u8) -> NumprResult<()> {
         b.set(pt, n)?;
-        self.masks[pt.index()] = 0;
-        self.popcnts[pt.index()] = 10;
+        self.masks[pt.index()] = 1;
         self.dec(pt, n)?;
         Ok(())
     }
 
     fn dec(&mut self, pt: Pt, n: u8) -> NumprResult<()> {
+        let b = !(1 << n);
         for p in PtIter::row(pt) {
-            let b = 1 << n;
-            let m = (self.masks[p.index()] & b) != 0;
-            self.masks[p.index()] &= !b;
-            self.popcnts[p.index()] -= m as u8;
-            if self.popcnts[p.index()] == 0 {
+            self.masks[p.index()] &= b;
+            if self.masks[p.index()] == 0 {
                 return NumprError::unsolvable();
             }
         }
 
         for p in PtIter::block(pt) {
-            let b = 1 << n;
-            let m = (self.masks[p.index()] & b) != 0;
-            self.masks[p.index()] &= !b;
-            self.popcnts[p.index()] -= m as u8;
-            if self.popcnts[p.index()] == 0 {
+            self.masks[p.index()] &= b;
+            if self.masks[p.index()] == 0 {
                 return NumprError::unsolvable();
             }
         }
 
         for p in PtIter::col(pt) {
-            let b = 1 << n;
-            let m = (self.masks[p.index()] & b) != 0;
-            self.masks[p.index()] &= !b;
-            self.popcnts[p.index()] -= m as u8;
-            if self.popcnts[p.index()] == 0 {
+            self.masks[p.index()] &= b;
+            if self.masks[p.index()] == 0 {
                 return NumprError::unsolvable();
             }
         }
@@ -83,7 +71,10 @@ impl HeuristicSolver {
             let mut end = idx.len();
             for _ in 0..idx.len() {
                 let i = idx[k] as usize;
-                if self.popcnts[i] != 1 {
+
+                // Is only one bit (i.e. one number to put) left?
+                let m = self.masks[i] >> 1;
+                if m & (m - 1) != 0 {
                     k += 1;
                     continue;
                 }
@@ -91,7 +82,6 @@ impl HeuristicSolver {
                 end -= 1;
                 idx.swap(k, end);
 
-                let m = self.masks[i] >> 1;
                 let bit = LOG_TABLE_LOW[(m & 0xf) as usize]
                     + LOG_TABLE_HIGH[((m >> 4) & 0xf) as usize]
                     + ((m >> 8) * 9) as u8;
@@ -114,7 +104,6 @@ impl HeuristicSolver {
         for c in b.candidates(pt, random) {
             let mut b = *b;
             let masks = self.masks;
-            let popcnts = self.popcnts;
             if self.set(&mut b, pt, c).is_ok() {
                 let res = self.recurse(&mut b, idx, random);
                 if res.is_ok() {
@@ -122,7 +111,6 @@ impl HeuristicSolver {
                 }
             }
             self.masks = masks;
-            self.popcnts = popcnts;
         }
         NumprError::unsolvable()
     }
@@ -141,7 +129,7 @@ impl Solver for HeuristicSolver {
         {
             let mut k = 0;
             for i in 0..SIZE {
-                if self.popcnts[i] == 10 {
+                if self.masks[i] == 1 {
                     continue;
                 }
                 idx[k] = i as u8;
